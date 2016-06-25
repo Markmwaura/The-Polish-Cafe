bb752b9a9dde093e2c833d8fa7f4dac858b7c887
Mark Mwaura <markkmwaura@gmail.com>
2016-06-25 03:11:17 +0300

    initialised and added files v 1.0

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function focusOnElement(element_id) {
+
+    goToByScroll(element_id);
+}
+
+function goToByScroll(id) {
+
+    $('html,body').animate({
+            scrollTop: $("#" + id).offset().top
+        },
+        'slow');
+}
